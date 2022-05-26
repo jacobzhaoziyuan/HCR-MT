f49833b3e4297267b5991e35c1ed5f1516de277b
ZIYUAN (Jacob) ZHAO <jacobzhaoziyuan@gmail.com>
2022-05-26 19:01:26 +0800

    First Version

---
+++ code/test_util.py
@@ -0,0 +1,177 @@
+import h5py
+import math
+import nibabel as nib
+import numpy as np
+from medpy import metric
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+from scipy import ndimage
+
+
+def test_all_case(net, image_list, num_classes, patch_size=(112, 112, 80), stride_xy=18, stride_z=4, save_result=True, test_save_path=None, preproc_fn=None):
+    total_metric = 0.0
+    for image_path in tqdm(image_list):
+        id = image_path.split('/')[-1]
+        h5f = h5py.File(image_path, 'r')
+        image = h5f['image'][:]
+        label = h5f['label'][:]
+        if preproc_fn is not None:
+            image = preproc_fn(image)
+        prediction, score_map = test_single_case(net, image, stride_xy, stride_z, patch_size, num_classes=num_classes)
+        prediction = ndimage.binary_fill_holes(prediction)
+
+        if np.sum(prediction)==0:
+            single_metric = (0,0,0,0)
+        else:
+            single_metric = calculate_metric_percase(prediction, label[:])
+        total_metric += np.asarray(single_metric)
+
+        if save_result:
+            nib.save(nib.Nifti1Image(prediction.astype(np.float32), np.eye(4)), test_save_path + id + "_pred.nii.gz")
+            nib.save(nib.Nifti1Image(image[:].astype(np.float32), np.eye(4)), test_save_path + id + "_img.nii.gz")
+            nib.save(nib.Nifti1Image(label[:].astype(np.float32), np.eye(4)), test_save_path + id + "_gt.nii.gz")
+    avg_metric = total_metric / len(image_list)
+    print('average metric is {}'.format(avg_metric))
+
+    return avg_metric
+
+
+def test_single_case(net, image, stride_xy, stride_z, patch_size, num_classes=1):
+    w, h, d = image.shape
+
+    # if the size of image is less than patch_size, then padding it
+    add_pad = False
+    if w < patch_size[0]:
+        w_pad = patch_size[0]-w
+        add_pad = True
+    else:
+        w_pad = 0
+    if h < patch_size[1]:
+        h_pad = patch_size[1]-h
+        add_pad = True
+    else:
+        h_pad = 0
+    if d < patch_size[2]:
+        d_pad = patch_size[2]-d
+        add_pad = True
+    else:
+        d_pad = 0
+    wl_pad, wr_pad = w_pad//2,w_pad-w_pad//2
+    hl_pad, hr_pad = h_pad//2,h_pad-h_pad//2
+    dl_pad, dr_pad = d_pad//2,d_pad-d_pad//2
+    if add_pad:
+        image = np.pad(image, [(wl_pad,wr_pad),(hl_pad,hr_pad), (dl_pad, dr_pad)], mode='constant', constant_values=0)
+    ww,hh,dd = image.shape
+
+    sx = math.ceil((ww - patch_size[0]) / stride_xy) + 1
+    sy = math.ceil((hh - patch_size[1]) / stride_xy) + 1
+    sz = math.ceil((dd - patch_size[2]) / stride_z) + 1
+    print("{}, {}, {}".format(sx, sy, sz))
+    score_map = np.zeros((num_classes, ) + image.shape).astype(np.float32)
+    cnt = np.zeros(image.shape).astype(np.float32)
+
+    for x in range(0, sx):
+        xs = min(stride_xy*x, ww-patch_size[0])
+        for y in range(0, sy):
+            ys = min(stride_xy * y,hh-patch_size[1])
+            for z in range(0, sz):
+                zs = min(stride_z * z, dd-patch_size[2])
+                test_patch = image[xs:xs+patch_size[0], ys:ys+patch_size[1], zs:zs+patch_size[2]]
+                test_patch = np.expand_dims(np.expand_dims(test_patch,axis=0),axis=0).astype(np.float32)
+                test_patch = torch.from_numpy(test_patch).cuda()
+                y1 = net(test_patch)
+                y = F.softmax(y1, dim=1)
+                y = y.cpu().data.numpy()
+                y = y[0,:,:,:,:]
+                score_map[:, xs:xs+patch_size[0], ys:ys+patch_size[1], zs:zs+patch_size[2]] \
+                  = score_map[:, xs:xs+patch_size[0], ys:ys+patch_size[1], zs:zs+patch_size[2]] + y
+                cnt[xs:xs+patch_size[0], ys:ys+patch_size[1], zs:zs+patch_size[2]] \
+                  = cnt[xs:xs+patch_size[0], ys:ys+patch_size[1], zs:zs+patch_size[2]] + 1
+    score_map = score_map/np.expand_dims(cnt,axis=0)
+    label_map = np.argmax(score_map, axis = 0)
+    if add_pad:
+        label_map = label_map[wl_pad:wl_pad+w,hl_pad:hl_pad+h,dl_pad:dl_pad+d]
+        score_map = score_map[:,wl_pad:wl_pad+w,hl_pad:hl_pad+h,dl_pad:dl_pad+d]
+    return label_map, score_map
+
+
+def CRFs(original_image_path,predicted_image_path,CRF_image_path):
+    print("original_image_path: ",original_image_path)
+    img = imread(original_image_path)
+    
+    # 将predicted_image的RGB颜色转换为uint32颜色 0xbbggrr
+    anno_rgb = imread(predicted_image_path).astype(np.uint32)
+    anno_lbl = anno_rgb[:,:,0] + (anno_rgb[:,:,1] << 8) + (anno_rgb[:,:,2] << 16)
+    
+    # 将uint32颜色转换为1,2,...
+    colors, labels = np.unique(anno_lbl, return_inverse=True)
+    
+    # 如果你的predicted_image里的黑色（0值）不是待分类类别，表示不确定区域，即将分为其他类别
+    # 那么就取消注释以下代码
+    #HAS_UNK = 0 in colors
+    #if HAS_UNK:
+    #colors = colors[1:]
+    
+    # 创建从predicted_image到32位整数颜色的映射。
+    colorize = np.empty((len(colors), 3), np.uint8)
+    colorize[:,0] = (colors & 0x0000FF)
+    colorize[:,1] = (colors & 0x00FF00) >> 8
+    colorize[:,2] = (colors & 0xFF0000) >> 16
+    
+    # 计算predicted_image中的类数。
+    n_labels = len(set(labels.flat))
+    #n_labels = len(set(labels.flat)) - int(HAS_UNK) ##如果有不确定区域，用这一行代码替换上一行
+
+    d = dcrf.DenseCRF(img.shape[1] * img.shape[0], n_labels)
+
+    # 得到一元势（负对数概率）
+    U = unary_from_labels(labels, n_labels, gt_prob=0.5, zero_unsure=None)  
+    #U = unary_from_labels(labels, n_labels, gt_prob=0.7, zero_unsure=HAS_UNK)## 如果有不确定区域，用这一行代码替换上一行
+    d.setUnaryEnergy(U)
+
+    # 这将创建与颜色无关的功能，然后将它们添加到CRF中
+    feats = create_pairwise_gaussian(sdims=(3, 3), shape=img.shape[:2])
+    d.addPairwiseEnergy(feats, compat=8,kernel=dcrf.DIAG_KERNEL,
+                        normalization=dcrf.NORMALIZE_SYMMETRIC)
+
+    # 这将创建与颜色相关的功能，然后将它们添加到CRF中
+    feats = create_pairwise_bilateral(sdims=(80, 80), schan=(13, 13, 13),
+                                      img=img, chdim=2)
+    d.addPairwiseEnergy(feats, compat=10,
+                        kernel=dcrf.DIAG_KERNEL,
+                        normalization=dcrf.NORMALIZE_SYMMETRIC)
+   
+    Q = d.inference(10)
+    
+    # 找出每个像素最可能的类
+    MAP = np.argmax(Q, axis=0)
+    
+    # 将predicted_image转换回相应的颜色并保存图像
+    MAP = colorize[MAP,:]
+    MAP = MAP.reshape(img.shape)
+    # imwrite(CRF_image_path, MAP.reshape(img.shape))
+    # print("CRF图像保存在",CRF_image_path,"!")
+
+
+def cal_dice(prediction, label, num=2):
+    total_dice = np.zeros(num-1)
+    for i in range(1, num):
+        prediction_tmp = (prediction==i)
+        label_tmp = (label==i)
+        prediction_tmp = prediction_tmp.astype(np.float)
+        label_tmp = label_tmp.astype(np.float)
+
+        dice = 2 * np.sum(prediction_tmp * label_tmp) / (np.sum(prediction_tmp) + np.sum(label_tmp))
+        total_dice[i - 1] += dice
+
+    return total_dice
+
+
+def calculate_metric_percase(pred, gt):
+    dice = metric.binary.dc(pred, gt)
+    jc = metric.binary.jc(pred, gt)
+    hd = metric.binary.hd95(pred, gt)
+    asd = metric.binary.asd(pred, gt)
+
+    return dice, jc, hd, asd
---
+++ data/README.md
@@ -0,0 +1,6 @@
+* Download the GE-MRI: dataset from [2018 Atrial Segmentation Chanllenge](http://atriaseg2018.cardiacatlas.org/)
+
+    * We evaluate our proposed semi-supervised method on the dataset of 2018 Atrial Segmentation Challenge for left atrium segmentation in 3D gadolinium-enhanced MR image scans (GE-MRIs). The dataset consists of 100 scans with segmentation masks. We select 80 samples for training and 20 for testing and use the same data preprocessing methods for a fair comparison.
+    * The data can be downloaded [here](https://github.com/yulequan/UA-MT/tree/master/data).
+
+
